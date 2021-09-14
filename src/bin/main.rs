2dae094efc978b550ec3c09d444e08ac7ccb490e
Cristobal <correodecristobal@gmail.com>
2021-09-14 12:04:53 -0600

    added set random attribute, set attribute options

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,26 @@
 use rust_nft_generator::ItemGenerator;
+use rust_nft_generator::AttributeOptions;
 
 fn main() {
-    let new_item = ItemGenerator::new();
+    let mut new_item = ItemGenerator::new();
     println!("new item is: {:?}", new_item);
+    //TODO: seems like I would need to create a constructor or new
+    // function to add the AttributeOptions
+    // - parse attribute options from file 
+    let mut attr_options_01 =
+        AttributeOptions{name:"eyes".to_string(),
+            attribute_options:vec!["eyes01".to_string(), "eyes02".to_string()], selected_attribute: None};
+    let mut attr_options_02 =
+        AttributeOptions{name:"mouth".to_string(),
+            attribute_options:vec!["mouth01".to_string(), "mouth02".to_string()], selected_attribute: None};
+
+    attr_options_01.set_random_attribute();
+    attr_options_02.set_random_attribute();
+
+    let mut new_item_with_options = new_item.set_attribute_options(attr_options_01);
+
+    new_item_with_options = new_item_with_options.set_attribute_options(attr_options_02);
+    new_item_with_options.print_content();
+
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
 use std::fmt::Debug;
+use rand::seq::SliceRandom;
 
 #[derive(Debug)]
 pub struct ItemGenerator {
     attributes: Vec<AttributeOptions>,
-}
-
-#[derive(Debug)]
-pub struct AttributeOptions {
-    name: String,
-    attribute_options: Vec<String>,
 }
 
 impl ItemGenerator {
@@ -18,8 +13,42 @@ impl ItemGenerator {
         }
     }
 
-    pub fn setAttributeOptions(&mut self, attr_options: AttributeOptions) -> &mut ItemGenerator {
+    pub fn set_attribute_options(&mut self, attr_options: AttributeOptions) -> &mut ItemGenerator {
         self.attributes.push(attr_options);
         self
     }
+
+
+    pub fn print_content(&self) {
+        for attr in &self.attributes {
+            println!("attributes: {:?}", attr);
+        }
+    }
+}
+
+
+#[derive(Debug)]
+pub struct AttributeOptions {
+    pub name: String,
+    pub attribute_options: Vec<String>,
+    pub selected_attribute: Option<String>
+}
+
+impl AttributeOptions {
+    pub fn set_random_attribute(&mut self) -> &mut AttributeOptions {
+        // return one item from vec
+        let selected = self.attribute_options.choose(&mut rand::thread_rng());
+        match selected {
+            Some(_) => {
+                // unwrap then put in option
+                let selection = selected.unwrap();
+                println!("selected is {:?}", selected);
+                self.selected_attribute = Some(selection.to_owned());
+            },
+            None => println!("no selected")
+        }
+        // set into selected_attribute field
+        self
+    }
+
 }
