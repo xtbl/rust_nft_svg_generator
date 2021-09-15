8426b5afb8e10ef1b230edd19c6bf444779fe128
Cristobal <correodecristobal@gmail.com>
2021-09-14 21:37:30 -0600

    working PoC, wip

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,23 +4,43 @@ use rust_nft_generator::AttributeOptions;
 fn main() {
     let mut new_item = ItemGenerator::new();
     println!("new item is: {:?}", new_item);
-    //TODO: seems like I would need to create a constructor or new
-    // function to add the AttributeOptions
-    // - parse attribute options from file 
-    let mut attr_options_01 =
-        AttributeOptions{name:"eyes".to_string(),
-            attribute_options:vec!["eyes01".to_string(), "eyes02".to_string()], selected_attribute: None};
-    let mut attr_options_02 =
-        AttributeOptions{name:"mouth".to_string(),
-            attribute_options:vec!["mouth01".to_string(), "mouth02".to_string()], selected_attribute: None};
+    //TODO:
+    // x seems like I would need to create a constructor or new
+        // function to add the AttributeOptions
+    // x read attributeoptions from file
+    // open template svg file
+    // replace template spaces with attribute content
+    // save new file
+
+
+    // setup AttributeOptions structs
+    let mut attr_options_01 = AttributeOptions{name:"eyes".to_string(), attribute_options:vec![], selected_attribute: None};
+    let mut attr_options_02 = AttributeOptions{name:"mouth".to_string(), attribute_options:vec![], selected_attribute: None};
+    let mut attr_options_03 = AttributeOptions{name:"hat".to_string(), attribute_options:vec![], selected_attribute: None};
+
+    let attribute_files_eyes = vec!["generative_templates/attribute_options_eyes_01.txt",
+        "generative_templates/attribute_options_eyes_02.txt"];
+    let attribute_files_mouth = vec!["generative_templates/attribute_options_mouth_01.txt",
+        "generative_templates/attribute_options_mouth_02.txt"];
+    let attribute_files_hat = vec!["generative_templates/attribute_options_hat_01.txt",
+        "generative_templates/attribute_options_hat_02.txt"];
+
+    // load file content
+    attr_options_01.load_attribute_options("eyes".to_string(), attribute_files_eyes);
+    attr_options_02.load_attribute_options("mouth".to_string(), attribute_files_mouth);
+    attr_options_03.load_attribute_options("hat".to_string(), attribute_files_hat);
 
     attr_options_01.set_random_attribute();
     attr_options_02.set_random_attribute();
+    attr_options_03.set_random_attribute();
 
     let mut new_item_with_options = new_item.set_attribute_options(attr_options_01);
-
     new_item_with_options = new_item_with_options.set_attribute_options(attr_options_02);
+    new_item_with_options = new_item_with_options.set_attribute_options(attr_options_03);
+
     new_item_with_options.print_content();
+    new_item_with_options.generate_file();
+
 
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 use std::fmt::Debug;
 use rand::seq::SliceRandom;
+use std::fs;
+use std::fs::File;
+use std::path::Path;
+use std::io::Write;
 
 #[derive(Debug)]
 pub struct ItemGenerator {
@@ -18,11 +22,53 @@ impl ItemGenerator {
         self
     }
 
-
     pub fn print_content(&self) {
+        println!("ItemGenerator -----------------");
         for attr in &self.attributes {
-            println!("attributes: {:?}", attr);
+            println!("selected_attribute: {:?}", attr.selected_attribute);
         }
+    }
+
+    pub fn generate_file(&self) {
+        // read file
+        let mut file_contents = fs::read_to_string("generative_templates/generative_template_01.svg").expect("Error reading file");
+        println!("-------- filecontents {}", file_contents);
+
+        // replace contents with attributes
+        for attr in &self.attributes {
+            match attr.name.as_str() {
+                "eyes" => {
+                    file_contents = file_contents.replace("$EYES$", &attr.selected_attribute.as_ref().unwrap());
+                },
+                "mouth" => {
+                    file_contents = file_contents.replace("$MOUTH$", &attr.selected_attribute.as_ref().unwrap());
+                },
+                "hat" => {
+                    file_contents = file_contents.replace("$HAT$", &attr.selected_attribute.as_ref().unwrap());
+                },
+                _ => {
+                    ()
+                }
+            }
+        }
+        println!("-------- filecontents {}", file_contents);
+
+        // TODO:
+        // set to generate n new files instead of hardcoded name
+
+        let path = Path::new("generated_07.svg");
+        let display = path.display();
+
+        let mut file = match File::create(&path) {
+            Err(why) => panic!("couldn't create {}: {}", display, why),
+            Ok(file) => file,
+        };
+
+        match file.write_all(file_contents.as_bytes()) {
+            Err(why) => panic!("couldn't write to {}: {}", display, why),
+            Ok(_) => println!("successfully wrote to {}", display),
+        }
+
     }
 }
 
@@ -35,6 +81,23 @@ pub struct AttributeOptions {
 }
 
 impl AttributeOptions {
+    pub fn get_attributes_from_file(&mut self, filename: &str) -> String {
+        let file_contents = fs::read_to_string(filename).expect("Error reading file");
+        println!("file contents is: {}", file_contents);
+        // TODO: push file_contents into attribute_options
+        file_contents
+    }
+
+    pub fn load_attribute_options(&mut self, attribute_name: String, file_names: Vec<&str>) {
+        self.name = attribute_name;
+        let mut loaded_attributes = vec![];
+        for file_name in file_names {
+            loaded_attributes.push(self.get_attributes_from_file(file_name));
+        }
+        println!("loaded_attributes: {:?}", loaded_attributes);
+        self.attribute_options = loaded_attributes;
+    }
+
     pub fn set_random_attribute(&mut self) -> &mut AttributeOptions {
         // return one item from vec
         let selected = self.attribute_options.choose(&mut rand::thread_rng());
